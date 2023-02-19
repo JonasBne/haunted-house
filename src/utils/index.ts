0aa5700c4e39b635d5eb127054c9c06aa577e8a2
Jonas Boone <boone.jonas@outlook.be>
2023-02-19 15:14:38 +0100

    feat(walls): add texture

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 
+const textureLoader = new THREE.TextureLoader();
+
 export const createFloor = () => {
   const geo = new THREE.PlaneGeometry(20, 20);
   const material = new THREE.MeshStandardMaterial({ color: '#a9c388' });
@@ -8,9 +10,13 @@ export const createFloor = () => {
 };
 
 export const createHouse = () => {
+  const wallsTexture = textureLoader.load(
+    '../../static/textures/bricks/color.jpg',
+  );
+
   const walls = new THREE.Mesh(
     new THREE.BoxGeometry(5, 5, 5),
-    new THREE.MeshStandardMaterial(),
+    new THREE.MeshStandardMaterial({ map: wallsTexture }),
   );
 
   walls.position.y = 0.5;
