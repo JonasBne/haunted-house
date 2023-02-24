23418131078216133bb7929bdbdbba7b00038c99
Jonas Boone <boone.jonas@outlook.be>
2023-02-24 18:53:27 +0100

    refactor(debug): remove panel

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Stats from 'stats.js';
-import GUI from 'lil-gui';
 import { createFloor, createGhosts, createGraves, createHouse } from './utils';
 import { colors } from './consts';
 import { Clock } from 'three';
@@ -11,9 +10,6 @@ import { Clock } from 'three';
 const stats = new Stats();
 stats.showPanel(0);
 document.body.appendChild(stats.dom);
-
-// debug
-const gui = new GUI();
 
 // canvas
 const canvas = document.getElementsByClassName('webgl')[0] as HTMLCanvasElement;
@@ -51,17 +47,12 @@ scene.add(ghost1, ghost2, ghost3);
 // Ambient light
 const ambientLight = new THREE.AmbientLight(colors.light, 0.12);
 ambientLight.castShadow = true;
-gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
 scene.add(ambientLight);
 
 // Directional light
 const moonLight = new THREE.DirectionalLight(colors.light, 0.12);
 moonLight.castShadow = true;
 moonLight.position.set(4, 5, -2);
-gui.add(moonLight, 'intensity').min(0).max(1).step(0.001);
-gui.add(moonLight.position, 'x').min(-5).max(5).step(0.001);
-gui.add(moonLight.position, 'y').min(-5).max(5).step(0.001);
-gui.add(moonLight.position, 'z').min(-5).max(5).step(0.001);
 scene.add(moonLight);
 
 // fog
